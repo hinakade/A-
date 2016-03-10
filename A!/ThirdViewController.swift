7237c04bc18709296d56d93062f3ba922f8c6265
kadehina <geta206@gmail.com>
2016-03-10 16:47:30 +0800

    おまけが表示されない未解決。tableview製作中。

--- A!/ThirdViewController.swift
+++ A!/ThirdViewController.swift
@@ -7,11 +7,19 @@
 //
 
 import UIKit
+//AVFoundationを追加
+import AVFoundation
 
-class ThirdViewController: UIViewController {
+//音声クラス宣言
+var myAudioPlayer : AVAudioPlayer!
+var myButton : UIButton!
+
+
+class ThirdViewController: UIViewController,AVAudioPlayerDelegate  {
     @IBOutlet weak var point: UITextField!
     @IBOutlet weak var comment: UITextField!
     @IBOutlet weak var backTitleBtn: SpringButton!
+    
     
     override func viewWillAppear(animated: Bool) {
         
@@ -47,6 +55,24 @@ class ThirdViewController: UIViewController {
     
         
     }
+    
+    @IBAction func backBtnTap(sender: AnyObject) {
+        //再生する音源のURLを生成.
+        let soundFilePath : NSString = NSBundle.mainBundle().pathForResource("btnstart", ofType: "mp3")!
+        let fileURL : NSURL = NSURL(fileURLWithPath: soundFilePath as String)
+        
+        //AVAudioPlayerのインスタンス化.
+        myAudioPlayer = try!(AVAudioPlayer(contentsOfURL: fileURL, fileTypeHint: nil))
+        
+        //AVAudioPlayerのデリゲートをセット.
+        myAudioPlayer.delegate = self
+        
+        myAudioPlayer.play()
+        print("ボタンタップ")
+    }
+
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
